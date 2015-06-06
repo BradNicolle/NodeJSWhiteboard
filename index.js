20e07b394d93b586afb9054dbbc97de012eb774c
BradNicolle <bradnicolle@gmail.com>
2015-06-06 23:55:01 +0930

    Added user counter, changed dots to lines

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var num_users = 0;
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static('public'));
@@ -13,13 +15,33 @@ app.get('/', function(request, response) {
 
 io.on('connection', function(socket) {
 	console.log('a user connected');
-	socket.on('l', function(msg) {
+	num_users++;
+	updateUsers();
+
+	socket.on('move', function(msg) {
 		console.log(msg);
-		socket.broadcast.emit('l', msg);
+		var parsed = JSON.parse(msg);
+		socket.broadcast.emit('move', '{"id":"' + socket.id + '","x":' + parsed.x + ',"y":' + parsed.y + '}');
 	});
+
+	socket.on('release', function(msg) {
+		console.log(socket.id + " released");
+		socket.broadcast.emit('release', socket.id);
+	});
+
+	socket.on('disconnect', function() {
+		console.log('a user disconnected');
+		num_users--;
+		updateUsers();
+	});
+
 });
 
 
 http.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+function updateUsers() {
+	io.emit('users', num_users);
+}
--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,5 +1,7 @@
 var down = false;
 
+var userMap = {};
+
 function onLoad() {
 
 	var socket = io();
@@ -10,9 +12,30 @@ function onLoad() {
 
 	var ctx = canvas.getContext("2d");
 
-	socket.on('l', function(msg) {
+//	ctx.moveTo(0, 0);
+
+	socket.on('move', function(msg) {
 		var parsed = JSON.parse(msg);
-		ctx.fillRect(parsed.x, parsed.y, 3, 3);
+		if (userMap[parsed.id] == null) {
+			ctx.beginPath();
+		}
+		else {
+			var user = userMap[parsed.id];
+			ctx.moveTo(user.x, user.y);
+		}
+		userMap[parsed.id] = parsed;
+		ctx.lineTo(parsed.x, parsed.y);
+		ctx.stroke();
+	});
+
+	socket.on('users', function(msg) {
+		document.getElementById("counter_text").innerText = msg;
+	});
+
+	socket.on('release', function(msg) {
+		if (msg in userMap) {
+			userMap[msg] = null;
+		}
 	});
 
 	addEventListener("mousedown", function(e) {
@@ -22,12 +45,15 @@ function onLoad() {
 
 	addEventListener("mouseup", function(e) {
 		down = false;
+		socket.emit('release');
 	}, false);
 
 	addEventListener("mousemove", function(e) {
 		if (down) {
-			ctx.fillRect(e.pageX, e.pageY, 3, 3);
-			socket.emit('l', '{"x":' + e.pageX + ',"y":' + e.pageY + '}');
+			ctx.lineTo(e.pageX, e.pageY);
+			ctx.stroke();
+
+			socket.emit('move', '{"x":' + e.pageX + ',"y":' + e.pageY + '}');
 		}
 	}, false);
 
